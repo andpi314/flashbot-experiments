9639578602f751c73dd949baba3ff5158f5ae8c2
Andrea Provino <andrea.provino.dev@gmail.com>
2022-02-13 13:30:09 +0100

    wip deploy contract

---
+++ src/local-tester/index.tsx
@@ -0,0 +1,9 @@
+import { FlashbotsBundleProvider } from "@flashbots/ethers-provider-bundle"
+import { BigNumber, providers, Wallet } from "ethers"
+
+import dotenv from "dotenv"
+import { GWEI } from "../ethreum.consts"
+
+dotenv.config()
+
+export default async function LocalTester() {}
